657edb8b168dfcafc176171038d3ea2dfd2c3ddd
Lachlan Ridley <lachie.ridley@gmail.com>
2024-05-25 20:52:01 +1200

    Add a simple AI test (more to follow)

--- source/test.lua
+++ source/test.lua
@@ -1,3 +1,10 @@
-function TestAdd()
-    luaunit.assertEquals(1, 3)
+import "board"
+import "ai"
+
+function TestPlaysCentreOnEmptyBoard()
+    local board = Board()
+
+    local aiMove = ChooseAiMove(board, "x")
+
+    luaunit.assertEquals(aiMove, { x = 2, y = 2 })
 end
